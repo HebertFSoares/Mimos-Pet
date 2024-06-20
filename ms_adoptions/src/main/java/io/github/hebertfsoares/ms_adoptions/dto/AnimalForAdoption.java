e9160a8bb245c9ecc6bf0dd75ff44956625289a9
Hebert Soares <hebertsoaresof@gmail.com>
2024-06-19 23:16:07 -0300

    Configurando o Microservice de Adoption e Configurando a Comunicação com RabbitMQ

---
+++ ms_adoptions/src/main/java/io/github/hebertfsoares/ms_adoptions/dto/AnimalForAdoption.java
@@ -0,0 +1,11 @@
+package io.github.hebertfsoares.ms_adoptions.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AnimalForAdoption {
+    private Long id;
+    private String name;
+}
